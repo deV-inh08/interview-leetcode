68902a74677d41efee51e57cce991313e7f262c9
deV-ing08 <vintd2004@gmail.com>
2025-01-03 15:50:10 +0700

    h-index

---
+++ h-index.js
@@ -0,0 +1,16 @@
+var hIndex = function(citations) {
+    // Bước 1: Sắp xếp mảng theo thứ tự giảm dần
+citations.sort((a, b) => b - a);
+
+// Bước 2: Tìm giá trị h-index
+let h = 0;
+for (let i = 0; i < citations.length; i++) {
+    if (citations[i] >= i + 1) {
+        h = i + 1;
+    } else {
+        break;
+    }
+}
+return h;
+};
+
