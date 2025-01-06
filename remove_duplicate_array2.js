09a4cac1a4393cac93c402ec7ba1e5033f4754b7
deV-ing08 <vintd2004@gmail.com>
2025-01-06 14:22:39 +0700

    remove duplicate array

--- remove_duplicate_array2.js
+++ remove_duplicate_array2.js
@@ -1,3 +1,11 @@
-var removeDuplicates = function(nums) {
-    
-};
+function removeDuplicates(nums) {
+    let write = 0; // Vị trí cần ghi tiếp theo
+    for (let read = 0; read < nums.length; read++) {
+        // Nếu chưa có 2 phần tử giống nhau trước đó
+        if (write < 2 || nums[read] !== nums[write - 2]) {
+            nums[write] = nums[read];
+            write++;
+        }
+    }
+    return write; // Số lượng phần tử hợp lệ sau khi xử lý
+}
