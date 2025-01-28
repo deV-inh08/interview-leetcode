e0db8e2ec90be2b3a60c2836fa35abdcfbaad207
deV-ing08 <vintd2004@gmail.com>
2025-01-28 13:32:52 +0700

    gcd_of_string

---
+++ gcd_of_string.js
@@ -0,0 +1,17 @@
+var gcdOfStrings = function(str1, str2) {
+    if(str1 + str2 !== str2 + str1) {
+        return ''
+    }
+
+    if(str1 === '' || str2 === '') {
+        return str1 || str2
+    }
+
+    if(str1.length > str2.length) {
+        return gcdOfStrings(str2, str1.slice(str2.length))
+    } else {
+        return gcdOfStrings(str1, str2.slice(str1.length))
+    }
+};
+
+console.log(gcdOfStrings("ABCABC", "ABC"))
